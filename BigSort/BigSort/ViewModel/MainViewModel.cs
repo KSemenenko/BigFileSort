691dcaad5548dacfaa9d65c5452150d9dd8b3af0
Kostiantyn Semenenko <kostiantyn.semenenko@e-consulting.com.ua>
2016-10-24 14:53:44 +0300

    generate text file

--- BigSort/BigSort/ViewModel/MainViewModel.cs
+++ BigSort/BigSort/ViewModel/MainViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using MVVMBase;
 
@@ -45,7 +48,24 @@ namespace BigSort.ViewModel
             {
                 return new DelegateCommand(executedParam =>
                 {
-                    // Do something
+                    string path = "d:\\WriteLines.txt";
+
+                    using (StreamWriter outputFile = new StreamWriter(path))
+                    {
+                        while(GetFreeSize(path) && GeteFileSize(path) < FileSize)
+                        { 
+                            foreach (var item in Helper.WordsDictionary)
+                            {
+                                outputFile.WriteLine(item.Key+"."+item.Value);
+                            }
+              
+                            Debug.WriteLine("size:"+ GeteFileSize(path));
+                        }
+
+                    }
+
+                    MessageBox.Show("ok");
+
                 },
                 canExecutedParam => { return !string.IsNullOrEmpty(FilePath) && FileSize > 0; });
             }
@@ -61,6 +81,17 @@ namespace BigSort.ViewModel
                 },
                 canExecutedParam => { return !string.IsNullOrEmpty(FilePath); });
             }
+        }
+
+        public int GeteFileSize(string path)
+        {                                                                //KB   //MB   //GB      
+            return Convert.ToInt32(new System.IO.FileInfo(path).Length / 1024 / 1024 / 1024);
+        }
+
+        public bool GetFreeSize(string path)
+        {                                                                    
+            DriveInfo driveInfo = new DriveInfo(path.Substring(0,2));
+            return (driveInfo.AvailableFreeSpace/1024/1024) > 500;
         }
 
     }
