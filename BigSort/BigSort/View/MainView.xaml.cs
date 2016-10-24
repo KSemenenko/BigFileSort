b40aa2a1b6fb5aec9a044ccf8f7bd290aacfd556
Kostiantyn Semenenko <kostiantyn.semenenko@e-consulting.com.ua>
2016-10-24 11:49:17 +0300

    view model

---
+++ BigSort/BigSort/View/MainView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using BigSort.ViewModel;
+
+namespace BigSort.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new MainViewModel();
+            this.DataContext = model;
+        }
+    }
+}
---
+++ BigSort/BigSort/ViewModel/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVMBase;
+
+namespace BigSort.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string filePath;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set
+            {
+                filePath = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(CreateFileCommand));
+                OnPropertyChanged(nameof(SortFileCommand));
+            }
+        }
+
+
+        private int fileSize;
+
+        public int FileSize
+        {
+            get { return fileSize; }
+            set
+            {
+                fileSize = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(CreateFileCommand));
+                OnPropertyChanged(nameof(SortFileCommand));
+            }
+        }
+
+        public ICommand CreateFileCommand
+        {
+            get
+            {
+                return new DelegateCommand(executedParam =>
+                {
+                    // Do something
+                },
+                canExecutedParam => { return !string.IsNullOrEmpty(FilePath) && FileSize > 0; });
+            }
+        }
+
+        public ICommand SortFileCommand
+        {
+            get
+            {
+                return new DelegateCommand(executedParam =>
+                {
+                    // Do something
+                },
+                canExecutedParam => { return !string.IsNullOrEmpty(FilePath); });
+            }
+        }
+
+    }
+}
