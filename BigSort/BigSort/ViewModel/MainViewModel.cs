078ff4c0776dc9fe1eee5f53da90957999772b98
Kostiantyn Semenenko <kostiantyn.semenenko@e-consulting.com.ua>
2016-10-24 15:54:24 +0300

    progress

--- BigSort/BigSort/ViewModel/MainViewModel.cs
+++ BigSort/BigSort/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,9 @@ namespace BigSort.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        private bool isFileGenerateInProgress = false;
+        private object sync = new object();
+
         private string filePath;
 
         public string FilePath
@@ -26,9 +30,10 @@ namespace BigSort.ViewModel
                 OnPropertyChanged(nameof(SortFileCommand));
             }
         }
+        
 
 
-        private int fileSize;
+        private int fileSize = 2;
 
         public int FileSize
         {
@@ -42,32 +47,38 @@ namespace BigSort.ViewModel
             }
         }
 
+
+        private int progress;
+
+        public int Progress
+        {
+            get { return progress; }
+            set
+            {
+                progress = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand CreateFileCommand
         {
             get
             {
-                return new DelegateCommand(executedParam =>
+                return new DelegateCommand(async executedParam =>
                 {
+                    if (isFileGenerateInProgress)
+                        return;
+
+                    isFileGenerateInProgress = true;
+
+                    OnPropertyChanged(nameof(CreateFileCommand));
+
                     string path = "d:\\WriteLines.txt";
-
-                    using (StreamWriter outputFile = new StreamWriter(path))
-                    {
-                        while(GetFreeSize(path) && GeteFileSize(path) < FileSize)
-                        { 
-                            foreach (var item in Helper.WordsDictionary)
-                            {
-                                outputFile.WriteLine(item.Key+"."+item.Value);
-                            }
-              
-                            Debug.WriteLine("size:"+ GeteFileSize(path));
-                        }
-
-                    }
-
-                    MessageBox.Show("ok");
+                    await GenerateFile(path);
+                    isFileGenerateInProgress = false;
 
                 },
-                canExecutedParam => { return !string.IsNullOrEmpty(FilePath) && FileSize > 0; });
+                canExecutedParam => { return !string.IsNullOrEmpty(FilePath) && FileSize > 0 && !isFileGenerateInProgress; });
             }
         }
 
@@ -88,11 +99,55 @@ namespace BigSort.ViewModel
             return Convert.ToInt32(new System.IO.FileInfo(path).Length / 1024 / 1024 / 1024);
         }
 
+        public int GeteFileSizeMB(string path)
+        {                                                                //KB   //MB        
+            return Convert.ToInt32(new System.IO.FileInfo(path).Length / 1024 / 1024);
+        }
+
         public bool GetFreeSize(string path)
         {                                                                    
             DriveInfo driveInfo = new DriveInfo(path.Substring(0,2));
             return (driveInfo.AvailableFreeSpace/1024/1024) > 500;
         }
 
+        public async Task GenerateFile(string path)
+        {
+            Debug.WriteLine("GenerateFile - Start");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int oldSize = -1;
+            Progress = 0;
+            int localFilesite = FileSize*1024*1024;
+            using (StreamWriter outputFile = new StreamWriter(path))
+            {
+                while (GetFreeSize(path) && GeteFileSize(path) < FileSize)
+                {
+                    Progress = localFilesite * GeteFileSizeMB(path) / 100;
+
+                    foreach (var item in Helper.WordsDictionary)
+                    {
+                        await outputFile.WriteLineAsync(item.Key + "." + item.Value);
+                    }
+
+                    var newSize = GeteFileSize(path);
+                    if(newSize > oldSize)
+                    {
+                        Debug.WriteLine("size:" + newSize);
+                        oldSize = newSize;
+                    }
+                    
+                }
+            }
+
+            sw.Stop();
+
+            if (!GetFreeSize(path))
+            {
+                MessageBox.Show("Disk if full :(");
+            }
+
+            MessageBox.Show("File ready - size is: " + GeteFileSize(path));
+            Debug.WriteLine("GenerateFile - Stop; Total:"+sw.Elapsed);
+        }
     }
 }
